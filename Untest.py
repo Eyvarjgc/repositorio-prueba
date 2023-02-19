2a65414a9857590b5a2a54b338f3ac3b9ec8aa12
Eyvarjgc <110841178+Eyvarjgc@users.noreply.github.com>
2023-02-19 12:07:19 -0500

    agregue funcion para guardar o no los datos(no finalizada)

--- Untest.py
+++ Untest.py
@@ -1,4 +1,6 @@
 #eval()
+
+import pickle
 class Persona:
     def __init__(self,nombre,edad) -> None:
         self.n = nombre
@@ -8,7 +10,19 @@ class Persona:
         print(f"Nombre: {self.n}")
         return f"Edad: {self.e}"
         
+    def add(self):
+        file = open("data" "wb")
+        pickle.dump("p1", "file")
+        file.close()
+        del file
 
+    def question(self):
+        ask = input("Desea guardar sus datos en un documento??(S/N)")
+        if ask.upper == "s":
+            p1.add()
+        else:
+            print("no desea guardar sus datos")
 
 p1 = Persona("Eyvar", "18")
 print(str(p1))
+p1.question()

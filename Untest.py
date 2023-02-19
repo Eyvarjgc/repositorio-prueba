99dcd931e129e5e1bab6c1a92f4b2a66e6541968
Eyvarjgc <110841178+Eyvarjgc@users.noreply.github.com>
2023-02-19 12:27:18 -0500

    Condicionales y datos finalizados

--- Untest.py
+++ Untest.py
@@ -1,5 +1,4 @@
 #eval()
-
 import pickle
 class Persona:
     def __init__(self,nombre,edad) -> None:
@@ -10,19 +9,28 @@ class Persona:
         print(f"Nombre: {self.n}")
         return f"Edad: {self.e}"
         
-    def add(self):
-        file = open("data" "wb")
-        pickle.dump("p1", "file")
-        file.close()
-        del file
 
     def question(self):
-        ask = input("Desea guardar sus datos en un documento??(S/N)")
-        if ask.upper == "s":
-            p1.add()
+        ask = input("Desea guardar sus datos en un documento??(S/N)").upper
+        if ask == "s" or "S":
+            file = open("data", "wb")
+            pickle.dump(p1,file)
+            file.close()
+            del file
+            print("-DATOS EXITOSAMENTE GUARDADOS-")
+
+            ask_2 = input("Desea leer los datos ingresados?(S/N)").upper
+            if ask_2 == "s" or "S":
+                file = open("data", "rb")
+                read = pickle.load(file)
+                print(read)
+            else:
+                print("No desea leer sus datos")
         else:
             print("no desea guardar sus datos")
 
+
+
 p1 = Persona("Eyvar", "18")
-print(str(p1))
 p1.question()
+
